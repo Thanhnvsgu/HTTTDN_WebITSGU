15024f073d74b0f426796a6a5fe2cddc484f07c3
minhphuc605 <43328859+minhphuc605@users.noreply.github.com>
2018-11-11 22:52:44 +0700

    them guest

---
+++ DoAnKhoaIT/DoAnKhoaIT/Areas/Guest/Controllers/ThongBaoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnKhoaIT.Areas.Guest.Controllers
+{
+    public class ThongBaoController : Controller
+    {
+        // GET: Guest/ThongBao
+        public ActionResult TB()
+        {
+            return View();
+        }
+        public ActionResult ThongBao1()
+        {
+            return View();
+        }
+    }
+}
