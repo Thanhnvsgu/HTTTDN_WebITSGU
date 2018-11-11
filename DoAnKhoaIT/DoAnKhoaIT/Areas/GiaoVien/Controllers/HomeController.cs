013350570d1ac6c1ffc253ae8838312441789395
minhphuc605 <43328859+minhphuc605@users.noreply.github.com>
2018-11-11 13:23:57 +0700

    Them giao vien

---
+++ DoAnKhoaIT/DoAnKhoaIT/Areas/GiaoVien/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnKhoaIT.Areas.GiaoVien.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: GiaoVien/Home
+        public ActionResult IndexGV()
+        {
+            return View();
+        }
+    }
+}
---
+++ DoAnKhoaIT/DoAnKhoaIT/Areas/GiaoVien/Controllers/ThongBaoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnKhoaIT.Areas.GiaoVien.Controllers
+{
+    public class ThongBaoController : Controller
+    {
+        // GET: GiaoVien/ThongBao
+        public ActionResult TB()
+        {
+            return View();
+        }
+        public ActionResult TB1()
+        {
+            return View();
+        }
+    }
+}
